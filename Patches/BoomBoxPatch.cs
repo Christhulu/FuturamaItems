5ead420b6de91a57dd5230b3a8cf0fdbbe7cf6b2
Chris Alexander <christopher.j.alexander@vanderbilt.edu>
2024-01-12 17:32:50 -0500

    update lib folder and add bender model

---
+++ Patches/BoomBoxPatch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+
+namespace FuturamaItems.Patches
+{
+    [HarmonyPatch(typeof(BoomboxItem))]
+    internal class BoomBoxPatch
+    {
+
+        [HarmonyPostfix]
+        static void songPatch()
+        {
+
+
+            //If currently held item is boombox, add the futurama theme song to the end?
+            //Maybe add the theme to the constructor
+            /* IEnumerable<BoomboxItem> boombox = RoundManager.;*/
+
+            RoundManager rm = UnityEngine.Object.FindObjectOfType<RoundManager>(); ;
+
+
+            /*IEnumerable<BoomboxItem> boombox = UnityEngine.G*/
+            AudioClip futuramaTheme = new AudioClip();
+
+            /*boombox.musicAudios.AddItem(futuramaTheme);*/
+
+
+
+            //Adding audio
+            //Make asset bundle from audio file using Asset Browser
+            //Load it in like video
+            //Add AudioClip
+            //Technically the boombox is a store bought item, and as a result we can check the start of the round or maybe in all player items to add the audio
+
+
+
+            //Spawning items is determined in the round manager with the SpawnScrapInLevel method. It gets spawnableScrab from the SelectableLevel component and goes into that level's
+            //List of spawnable scrap items until it hits a count
+            //So we'd have to add the scrap to each level and give it a rarity, a value, 
+            //Additionally, each item should extend GrabbableObject
+            //We can use the Item class and update the spawnPrefab with our models
+            //Additionally, there are audio clips for grabbing, dropping, pocketing, and throwing on the item that we can update with cool lines
+        }
+
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,14 +1,49 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
 
 namespace FuturamaItems
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+    public class FuturamaItemModBase : BaseUnityPlugin
     {
-        private void Awake()
+
+        private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+        private static FuturamaItemModBase Instance;
+
+        internal ManualLogSource mls;
+
+        public static AssetBundle benderModelAsset;
+
+        
+        void Awake()
         {
+            //Create static reference to mod instance
+            if(Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(PluginInfo.PLUGIN_GUID);
             // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            mls.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            //Load bender model
+            string sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            benderModelAsset = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, "bender"));
+            if (benderModelAsset == null)
+            {
+                mls.LogError("Failed to load custom assets."); // ManualLogSource for your plugin
+                return;
+            }
+
+            harmony.PatchAll(typeof(FuturamaItemModBase));
+
+
         }
     }
 }
