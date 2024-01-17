e885f822e548df7f0a9c7c9b0aaf56ab2487a4d6
Chris Alexander <christopher.j.alexander@vanderbilt.edu>
2024-01-17 12:20:58 -0500

    add some example sounds until I finish recording everything

--- Patches/ScrapPatch.cs
+++ Patches/ScrapPatch.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using GameNetcodeStuff;
@@ -24,17 +25,47 @@ namespace FuturamaItems.Patches
         [HarmonyPostfix]
         static void scrapPatch()
         {
-            RoundManager rm = UnityEngine.Object.FindObjectOfType<RoundManager>();
-
-            SelectableLevel currentLevel = rm.currentLevel;
-
             Item bender = FuturamaItemModBase.benderAssetBundle.LoadAsset<Item>("/Assets/benderbundled");
 
-            /*Items.RegisterScrap();*/
+            //Ensure audio doesn't play twice
+            Utilities.FixMixerGroups(bender.spawnPrefab);
+            
+            //This makes it so the other clients know what this object is
+            NetworkPrefabs.RegisterNetworkPrefab(bender.spawnPrefab);
+
+            //Update bender item stats
+            bender.isConductiveMetal = true;
+            bender.maxValue = 200;
+            bender.minValue = 40;
+
+            //I'm using these to take sounds from temporarily
+            Turret turret = new Turret();
+            Shovel shovel = new Shovel();
+            WalkieTalkie w = new WalkieTalkie();
+
+
+            //Update sound effects
+            bender.grabSFX = turret.chargingSFX;
+
+            var dropIndex = UnityEngine.Random.Range(0, shovel.hitSFX.Length);
+            bender.dropSFX = shovel.hitSFX[dropIndex];
+
+            var pocketIndex = UnityEngine.Random.Range(0, w.startTransmissionSFX.Length);
+            bender.pocketSFX = w.startTransmissionSFX[pocketIndex];
+
+            //Register item as scrap and as shop item
+            Items.RegisterScrap(bender, 300, Levels.LevelTypes.All);
+
+            TerminalNode node = ScriptableObject.CreateInstance<TerminalNode>();
+            node.clearPreviousText = true;
+            node.displayText = "Bite my shiny metal ass!\n\n";
+
+            Items.RegisterShopItem(bender, null, null, node, 0);
+
+            
 
 
 
-        
         }
         
 
